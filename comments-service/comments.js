ecfa2f012ee55490fc270d30d15f79a4c13e87cf
Jeremy Olea <jeremy.olea18@gmail.com>
2020-03-10 15:07:08 -0600

    Update comments.js

---
+++ comments-service/comments.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const db = require('./db');
+
+exports.plugin = {
+    pkg: require('./package.json'),
+    register: async function (server, options) {
+        // Example:
+        // server.route({
+        //     method: 'GET',
+        //     path: '/api/feed/',
+        //     handler: async function (request, h) {
+        //         try {
+        //             let page = parseInt(request.query.page) || 1;
+        //             const limit = parseInt(request.query.limit) || 9;
+        //             if (page < 1) page = 1;
+        //             const posts = await db.query(`
+        //                 SELECT * 
+        //                 FROM POSTS AS P, SCHOOL AS S, USER AS U
+        //                 WHERE P.User_ID=U.User_ID AND P.School_ID=S.School_ID
+        //                 ORDER BY Post_id
+        //                 LIMIT ${(page - 1) * limit}, ${limit}
+        //             `)
+        //             return { posts };
+        //         } catch(err) {
+        //             console.log(err);
+        //             // TODO: put in julian's error thing
+        //         }
+        //     }
+        // });
+    }
+};
