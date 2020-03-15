0cfa4d9f9f666445f8032199f49bbde5f900ae95
Oscar Wong <wong.oscar67@gmail.com>
2020-03-15 17:58:23 -0600

    per-school queries

--- main-app/school.js
+++ main-app/school.js
@@ -9,9 +9,9 @@ module.exports = [
     path: '/api/getSchools',
     handler: async function (request, h) {
       try {
-        const schoolNames = await db.query('SELECT SchoolName FROM SCHOOL');
+        const schools = await db.query('SELECT SchoolName, School_ID FROM SCHOOL');
         if (!schoolNames.length) throw new Error('Error getting school names!');
-        return helper.goodResponse(h, schoolNames);
+        return helper.goodResponse(h, schools);
       } catch (err) {
         return helper.badResponse(h, err);
       }
