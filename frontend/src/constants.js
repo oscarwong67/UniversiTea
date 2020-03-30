ab28f8aa08adab2e92b6e161f5955a471672d7e0
Ubuntu <ubuntu@ip-172-31-92-50.ec2.internal>
2020-03-30 00:02:41 +0000

    deployed again

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,2 @@
-// const API_ADDRESS = process.env.VUE_APP_API_ADDRESS;
-const API_ADDRESS = 'http://localhost:3000';
+const API_ADDRESS = process.env.VUE_APP_API_ADDRESS || 'http://localhost:3000';
 module.exports = { API_ADDRESS };
