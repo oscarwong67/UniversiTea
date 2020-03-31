dd81be6f46033c5f48c032d4309a43a667d8597b
JeremyOlea <43157357+JeremyOlea@users.noreply.github.com>
2020-03-31 15:53:16 -0600

    Post Model

--- posts-service/posts-controller.js
+++ posts-service/posts-controller.js
@@ -3,3 +3,18 @@
 const db = require('./db');
 const helper = require('./helper');
 const commentsMicroserviceHost = 'localhost:3002';
+
+exports.plugin = {
+    pkg: require('./package.json'),
+    register: async function (server, options) {
+      server.route({
+        method: 'GET',
+        path: '/api/feed',
+        handler: async function (request, h) {
+            //Write Code Here
+        }
+      });
+    }
+
+    
+  };
