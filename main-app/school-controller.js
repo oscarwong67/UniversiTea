adb8a982c3afcfce67aca66204336cb8aa8f93b9
JulianPinto <julianpinto@shaw.ca>
2020-03-31 20:04:54 -0600

    idfk

--- main-app/school-controller.js
+++ main-app/school-controller.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const helper = require('./helper');
 const schoolModel = require('./school-model');
 
 module.exports = [
@@ -10,13 +9,9 @@ module.exports = [
     // Output: array of all schools
     method: 'GET',
     path: '/api/getSchools',
-    async handler(request, h) {
-      try {
-        const schools = await schoolModel.getSchool();
-        return helper.goodResponse(h, schools);
-      } catch (err) {
-        return helper.badResponse(h, err);
-      }
+    handler: async function (request, h) {
+      const res = await schoolModel.getSchools(h);
+      return res;
     }
   },
   {
@@ -25,13 +20,9 @@ module.exports = [
     // Output: the school obj
     method: 'GET',
     path: '/api/getSchoolName/',
-    async handler(request, h) {
-      try {
-        const schoolNames = await schoolModel.getSchoolNames(request);
-        return helper.goodResponse(h, schoolNames);
-      } catch (err) {
-        return helper.badResponse(h, err);
-      }
+    handler: async function (request, h) {
+      const res = await schoolModel.getSchoolName(request, h);
+      return res;
     }
   }
 ]
--- main-app/school-model.js
+++ main-app/school-model.js
@@ -1,27 +1,27 @@
 'use strict'
 const db = require('./db');
+const helper = require('./helper');
 
-module.exports = [
-  {
-    //gets all schools in db
-    method: 'GET',
-    path: '/api/getSchools',
-    handler: async function () {
-      const schools = await db.query('SELECT SchoolName, School_ID FROM SCHOOL');
-      if (!schools.length) throw new Error('Error getting school names!');
-      return schools;
-    }
-  },
-
-  {
-    method: 'GET',
-    path: '/api/getSchoolName/',
-    handler: async function (request) {
-
-        let id = parseInt(request.query.schoolid);
-        const schools = await db.query('SELECT SchoolName FROM SCHOOL WHERE ?', { School_ID: id });
-        if (!schools.length) throw new Error('Error getting school names!');
-        return schools;
-    }
+const getSchools = async (h) => {
+  //gets all schools in db
+  try {
+    const schools = await db.query('SELECT SchoolName, School_ID FROM SCHOOL');
+    if (!schools.length) throw new Error('Error getting school names!');
+    return helper.goodResponse(h, schools);
+  } catch (err) {
+    return helper.badResponse(h, err);
   }
-]
+}
+
+const getSchoolName = async (request, h) => {
+  try {
+    let id = parseInt(request.query.schoolid);
+    const schools = await db.query('SELECT SchoolName FROM SCHOOL WHERE ?', { School_ID: id });
+    if (!schools.length) throw new Error('Error getting school names!');
+    return helper.goodResponse(h, schools);
+  } catch (err) {
+    return helper.badResponse(h, err);
+  }
+}
+
+module.exports = { getSchools, getSchoolName };
