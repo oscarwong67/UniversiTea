1c02f9f7dd641bab017b37c355649a3b3f98d3d9
JulianPinto <julianpinto@shaw.ca>
2020-03-31 19:14:07 -0600

    fixed?

--- main-app/school-controller.js
+++ main-app/school-controller.js
@@ -3,40 +3,35 @@
 const helper = require('./helper');
 const schoolModel = require('./school-model');
 
-exports.plugin = {
-  pkg: require('./package.json'),
-  register: async function (server, options) {
-    
+module.exports = [
+  {
     // gets all schools from db
     // Input: none
     // Output: array of all schools
-    server.route({
-      method: 'GET',
-      path: '/api/getSchools',
-      handler: async function (request, h) {
-        try {
-          const schools = await schoolModel.getSchool();
-          return helper.goodResponse(h, schools);
-        } catch (err) {
-          return helper.badResponse(h, err);
-        }
+    method: 'GET',
+    path: '/api/getSchools',
+    async handler(request, h) {
+      try {
+        const schools = await schoolModel.getSchool();
+        return helper.goodResponse(h, schools);
+      } catch (err) {
+        return helper.badResponse(h, err);
       }
-    });
-
+    }
+  },
+  {
     // searches for school by name in db
     // Input: schoolname
     // Output: the school obj
-    server.route({
-      method: 'GET',
-      path: '/api/getSchoolName/',
-      handler: async function (request, h) {
-        try {
-          const schoolNames = await schoolModel.getSchoolNames(request);
-          return helper.goodResponse(h, schoolNames);
-        } catch (err) {
-          return helper.badResponse(h, err);
-        }
+    method: 'GET',
+    path: '/api/getSchoolName/',
+    async handler(request, h) {
+      try {
+        const schoolNames = await schoolModel.getSchoolNames(request);
+        return helper.goodResponse(h, schoolNames);
+      } catch (err) {
+        return helper.badResponse(h, err);
       }
-    });
+    }
   }
-}
+]
