72168f08a37c088f6e1525c781259d8c7b8ef46c
andytran36 <andytran636@gmail.com>
2020-03-31 18:57:50 -0600

    Update admin-controller.js

--- main-app/admin-controller.js
+++ main-app/admin-controller.js
@@ -2,53 +2,49 @@
 
 const adminModel = require('./admin-model');
 
-exports.plugin = {
-  pkg: require('./package.json'),
-  register: async function (server, options) {
-    
-    server.route({
-      method: 'POST',
-      path: '/api/admin/promote',
-      async handler(request, h) {
-        const res = await adminModel.getUsers(request);
-        return res;
-      },
-    });
+module.exports = [
+  {
+    method: 'POST',
+    path: '/api/admin/promote',
+    async handler(request, h) {
+      const res = await adminModel.getUsers(request);
+      return res;
+    }
+  },
 
-    server.route({
-      method: 'GET',
-      path: '/api/admin/getusers',
-      async handler(request, h) {
-        const data = await adminModel.getUsers(request);
-        return data;
-      },
-    });
+  {
+    method: 'GET',
+    path: '/api/admin/getusers',
+    async handler(request, h) {
+      const data = await adminModel.getUsers(request);
+      return data;
+    },
+  },
 
-    server.route({
-      method: 'POST',
-      path: '/api/admin/addSchool',
-      async handler(request, h) {
-        const res = await adminModel.addSchool(request);
-        return res;
-      },
-    });
+  {
+    method: 'POST',
+    path: '/api/admin/addSchool',
+    async handler(request, h) {
+      const res = await adminModel.addSchool(request);
+      return res;
+    }
+  },
 
-    server.route({
-      method: 'POST',
-      path: '/api/admin/deleteSchool',
-      async handler(request, h) {
-        const res = await adminModel.deleteSchool(request);
-        return res;
-      },
-    });
+  {
+    method: 'POST',
+    path: '/api/admin/deleteSchool',
+    async handler(request, h) {
+      const res = await adminModel.deleteSchool(request);
+      return res;
+    },
+  },
 
-    server.route({
-      method: 'POST',
-      path: '/api/admin/editSchool',
-      async handler(request, h) {
-        const res = await adminModel.editSchool(request);
-        return res;
-      },
-    });
+  {
+    method: 'POST',
+    path: '/api/admin/editSchool',
+    async handler(request, h) {
+      const res = await adminModel.editSchool(request);
+      return res;
+    }
   }
-}
+]
